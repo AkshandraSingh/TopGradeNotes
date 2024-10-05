5b55a85d94dd3bce044d4069629b7da9e357b721
AkshandraSingh <nameste380@gmail.com>
2024-10-06 01:21:10 +0530

    Update: addedUpdateSubjectAPI

--- controllers/subjectController.js
+++ controllers/subjectController.js
@@ -8,18 +8,50 @@ module.exports = {
             const subjectData = new subjectModel(req.body)
             const userData = await userModel.findById(userId)
             if (userData.userRole !== "Admin") {
-                return res.status(403).json({
+                return res.status(403).send({
                     success: false,
                     message: 'Only admin users can create subjects',
                 })
             }
             await subjectData.save()
-            res.status(201).json({
+            res.status(201).send({
                 success: true,
                 message: 'Subject created successfully',
             })
         } catch (error) {
-            res.status(500).json({
+            res.status(500).send({
+                success: false,
+                message: 'Internal server error',
+                error: error.message,
+            })
+        }
+    },
+
+    updateSubject: async (req, res) => {
+        try {
+            const userId = req.params.userId
+            const subjectId = req.params.subjectId
+            const updatedSubjectData = req.body
+            const userData = await userModel.findById(userId)
+            if (userData.userRole !== "Admin") {
+                return res.status(403).send({
+                    success: false,
+                    message: 'Only admin users can create subjects',
+                })
+            }
+            const subjectData = await subjectModel.findByIdAndUpdate(subjectId, updatedSubjectData, { new: true })
+            if (!subjectData) {
+                return res.status(404).send({
+                    success: false,
+                    message: 'Subject not found',
+                })
+            }
+            res.status(200).send({
+                success: true,
+                message: 'Subject updated successfully',
+            })
+        } catch (error) {
+            res.status(500).send({
                 success: false,
                 message: 'Internal server error',
                 error: error.message,
--- routes/subjectRoute.js
+++ routes/subjectRoute.js
@@ -5,5 +5,6 @@ const subjectController = require('../controllers/subjectController')
 const subjectRouter = express.Router()
 
 subjectRouter.post('/createSubject/:userId', subjectController.createSubject)
+subjectRouter.patch('/updateSubject/:userId/:subjectId', subjectController.updateSubject)
 
 module.exports = subjectRouter
